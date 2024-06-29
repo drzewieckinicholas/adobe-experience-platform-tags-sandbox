d905445f0c1eaa407bb9dacebbb57a03800a2ee3
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2024-06-29 11:45:01 -0700

    feat(`apps/remix`): add a basic `Nav` component and clean up code from `create-remix`

--- apps/remix/app/root.tsx
+++ apps/remix/app/root.tsx
@@ -5,7 +5,8 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
-import './tailwind.css';
+
+import { Nav } from './components';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -26,5 +27,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <>
+      <Nav />
+      <Outlet />
+    </>
+  );
 }
