27cc7f39d72d799b84b89d09889cddf9abfdcdf9
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2024-06-29 11:40:39 -0700

    style(`apps/remix`): format files

--- apps/remix/app/root.tsx
+++ apps/remix/app/root.tsx
@@ -4,15 +4,15 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import "./tailwind.css";
+} from '@remix-run/react';
+import './tailwind.css';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
