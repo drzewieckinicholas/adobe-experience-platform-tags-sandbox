01edf983b7939dc499fefc8c30ee6b742d4c3349
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2024-06-30 20:48:15 -0700

    fix(`apps/remix`): do not render main if no children

--- apps/remix/app/components/Layout/index.tsx
+++ apps/remix/app/components/Layout/index.tsx
@@ -11,7 +11,7 @@ export default function Layout({ children, heading }: LayoutProps) {
     <>
       <WithNav />
       <Header heading={heading} />
-      <main>{children}</main>
+      {children && <main>{children}</main>}
     </>
   );
 }
