c37f958c743f6318f357d8a8fdc1e5a66e06a6ed
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2024-07-01 18:20:20 -0700

    feat(`libs/web-vitals`): add initial Web Vitals reporting functionality
    We minimize network usage by batching multiple metric reports together in a single network request.

--- libs/web-vitals/src/index.ts
+++ libs/web-vitals/src/index.ts
@@ -1,1 +1,61 @@
-export * from './lib/web-vitals';
+import { dataLayerPush, Events } from '@adobe-experience-platform-tags/tags';
+import { type Metric, onCLS, onFCP, onINP, onLCP, onTTFB } from 'web-vitals';
+
+let isInitialized = false;
+
+const queue: Set<Metric> = new Set();
+
+function addToQueue(metric: Metric) {
+  queue.add(metric);
+}
+
+function flushQueue() {
+  if (queue.size > 0) {
+    const metrics = Array.from(queue);
+
+    sendToDataLayer(metrics);
+
+    queue.clear();
+  }
+}
+
+function sendToDataLayer(metrics: Metric[]) {
+  metrics.forEach((metric) => {
+    const { delta, id, name, rating, value } = metric;
+
+    dataLayerPush({
+      event: Events.WebVitals,
+      metric: {
+        delta,
+        id,
+        name,
+        rating,
+        value,
+      },
+    });
+  });
+}
+
+export function initWebVitals() {
+  if (isInitialized) {
+    console.warn('Web Vitals is already initialized.');
+
+    return;
+  }
+
+  isInitialized = true;
+
+  onCLS(addToQueue);
+  onFCP(addToQueue);
+  onINP(addToQueue);
+  onLCP(addToQueue);
+  onTTFB(addToQueue);
+
+  addEventListener('visibilitychange', () => {
+    if (document.visibilityState === 'hidden') {
+      flushQueue();
+    }
+  });
+
+  addEventListener('pagehide', flushQueue);
+}
--- libs/web-vitals/src/lib/web-vitals.ts
+++
@@ -1,3 +0,0 @@
-export function webVitals(): string {
-  return 'web-vitals';
-}
